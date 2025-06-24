c7fdee9d832c8a3573a4a4bb1d84f1416e15b2c6
Shawn Rossbach <shawnscreed@gmail.com>
2025-06-23 23:11:57 -0400

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
-    'react-hooks/exhaustive-deps': 'off'
-  }
+    'react-hooks/exhaustive-deps': 'off' 
+  } 
 };
+ 
