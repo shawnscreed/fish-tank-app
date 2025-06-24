0efbd9444c08c22f3a93bdf50e8e4418e95299c4
Shawn Rossbach <shawnscreed@gmail.com>
2025-06-23 23:04:13 -0400

    update

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+// File: .eslintrc.js
+
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['next', 'next/core-web-vitals', 'plugin:@typescript-eslint/recommended'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'react-hooks/exhaustive-deps': 'off'
+  }
+};
