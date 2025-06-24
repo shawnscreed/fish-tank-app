ff6db708e416877314a7a1e2a68b469be05a289f
Shawn Rossbach <shawnscreed@gmail.com>
2025-06-24 17:43:46 -0400

    Add next.config.js for deployment compatibility

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  },
+};
+
+module.exports = nextConfig;
+
