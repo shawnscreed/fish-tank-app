8c08353498d54f577ea559c6b83d5dded0132e14
Shawn Rossbach <shawnscreed@gmail.com>
2025-06-23 23:15:34 -0400

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // ✅ Temporarily disable ESLint during production build
+  },
 };
 
 export default nextConfig;
