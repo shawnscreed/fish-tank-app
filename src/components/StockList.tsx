fd91c87679ccafa7d5599c52f700e19aec57c03c
Shawn Rossbach <shawnscreed@gmail.com>
2025-06-30 11:36:47 -0400

    update

--- src/components/StockList.tsx
+++ src/components/StockList.tsx
@@ -7,7 +7,7 @@ interface StockItem {
   name: string;
   type: string;
 }
-
+ 
 export default function StockList({ stock }: { stock: StockItem[] }) {
   const grouped = stock.reduce<Record<string, StockItem[]>>((acc, item) => {
     if (!acc[item.type]) acc[item.type] = [];
